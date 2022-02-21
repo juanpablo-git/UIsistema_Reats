c65fb3879fef4e2c72f6c9dc72d91a7f421c06ea
juanpablo-git <salesjuan781@gmail.com>
2022-02-21 14:44:10 -0300

    .

--- src/components/tolbar/index.js
+++ src/components/tolbar/index.js
@@ -3,24 +3,35 @@ import List from '@mui/material/List';
 import ListItemText from '@mui/material/ListItemText'
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ListItemButton from '@mui/material/ListItemButton'
+
+import "./style.css"
+
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import InboxIcon from '@mui/icons-material/Inbox';
+
+import { Link} from "react-router-dom"
 function Home() {
     return(
 <>
-<List  sx={{width: '100%', maxWidth: 200, bgcolor: 'background.paper' }}
-      aria-label="contacts">
-    {[{name:'Perfil',icon:<AccountCircleIcon htmlColor=" #4ba0ee"/>},
-    {name:'Chamados',icon:<InboxIcon htmlColor=" #4ba0ee" />}].map(item=>(
-        <ListItem disablePadding>
+<List 
+sx={{width: '100%', maxWidth: 200, bgcolor: 'background.paper' }}
+aria-label="contacts">
+    {[{name:'Perfil',icon:<AccountCircleIcon htmlColor=" #4ba0ee"/>,link:"/"},
+    {name:'Chamados',icon:<InboxIcon htmlColor=" #4ba0ee" />,
+    link:"/home"}].map(item=>(
+       <ListItem disablePadding>
+           <Link to={`${item.link}`} className="link-menu" >
             <ListItemButton>
             <ListItemIcon>
              {item.icon}
             </ListItemIcon>
             <ListItemText primary={`${item.name}`} /> 
-        </ListItemButton>
-        </ListItem>))}
+        </ListItemButton >
+        </Link>
+        </ListItem >
+       
+        ))}
 </List>
 </>
     )
